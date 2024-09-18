a4459a6e091d604671595399d988a2e58741ba9f
Victor Odhiambo <odhiamboondiko@gmail.com>
2024-09-19 00:08:05 +0300

    Update .gitignore to remove unnecessary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 function App() {
   return (
-    <div className='text-9xl text-red-700'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+
+    </Routes>
+    
+
+
+    </BrowserRouter>
   )
 }
 
