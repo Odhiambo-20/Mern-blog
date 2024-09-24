19fcad1cb877fe511ab858d3e25f0dddbd1a2967
Victor Odhiambo <odhiamboondiko@gmail.com>
2024-09-24 17:21:58 +0300

    complete sidebar of the dashboard

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log(mport.meta.env.VITE_FIREBASE_API_KEY);
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey:  import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-431e6.firebaseapp.com",
