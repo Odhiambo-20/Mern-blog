90bac32e12b4d45daab14be98941e6da9441d8b3
Victor Odhiambo <odhiamboondiko@gmail.com>
2024-09-25 23:03:37 +0300

    Add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,31 +1,34 @@
-import mongoose from "mongoose";
-import { type } from "os";
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+import mongoose from 'mongoose';
 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fprofile-image&psig=AOvVaw08nEtPAJNqJ8vXyBNFi1Dt&ust=1727261740013000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPjWvsa124gDFQAAAAAdAAAAABAE",
-
-    }
-    
-
-},{timestamps:true}
+    profilePicture: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-const User=mongoose.model('User',userSchema);
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
